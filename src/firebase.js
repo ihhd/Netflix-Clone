e7c0175d6011c7b8a361b869bfcf21c23e27fe61
ishwar-zeomega <ishwarhiremath2001@gmail.com>
2023-01-20 09:59:31 +0530

    Netflix-Clonr

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsJyARKFbUnvhh8CIVJtd1JoAXxGyp8gI",
+    authDomain: "netflix-clone-40e21.firebaseapp.com",
+    projectId: "netflix-clone-40e21",
+    storageBucket: "netflix-clone-40e21.appspot.com",
+    messagingSenderId: "71520368728",
+    appId: "1:71520368728:web:493eb6863ab8d4df10b22d"
+  };
